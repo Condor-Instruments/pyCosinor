61c67b2dab12e377667dcc61ebf9ad478983e13f
Julius Andretti <juliusandrettip3@gmail.com>
2019-10-29 14:51:27 -0300

    minor updates

--- test_final/final2.py
+++ test_final/final2.py
@@ -8,14 +8,15 @@ T = 24*60*60
 log = lr.LogRead('exlog.txt')
 t1 = log.makeT()[1][0:48*60]
 t2 = log.makeT(a=0)[1][0:48*60]
-activity = [(123 + 45*cos((2*pi/T)*i - pi/3)) for i in range(48*60)]
+activity = [(123 + 45*cos((2*pi/T)*i*60 - 1)) for i in range(48*60)]
 
 for i in range(24):
 	test1 = cs.Cosinor(t1[i*60:i*60+24*60],activity[i*60:i*60+24*60],T,alpha)
 	test1.fit()
 	test2 = cs.Cosinor(t2[i*60:i*60+24*60],activity[i*60:i*60+24*60],T,alpha)
 	test2.fit()
-	print('acroA='+str(test1.phi)+'; acroR='+str(test2.phi)+'; dif='+str(abs(test2.phi-test1.phi)))
+	print('acroA='+str(test1.phi)+'; acroR='+str(test2.phi) #+'; dif='+str(abs(test2.phi-test1.phi))
+		 )
 
 
 
